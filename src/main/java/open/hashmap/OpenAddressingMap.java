fc73b2f3526827debe4cb1305c7c69c2e3c2b504
ihPiontkovskyi <fuvkaperviy@gmail.com>
2020-02-22 12:09:37 +0200

    Add docs for contract

--- src/main/java/open/hashmap/OpenAddressingMap.java
+++ src/main/java/open/hashmap/OpenAddressingMap.java
@@ -1,11 +1,29 @@
 package open.hashmap;
 
 public interface OpenAddressingMap {
+    /**
+     * Used to put element in map
+     *
+     * @param key   int value of key
+     * @param value long value that stored by represented key
+     */
     void put(int key, long value);
 
+    /**
+     * Used to get element from map
+     *
+     * @param key int value of key
+     * @return long value that stored by represented key
+     */
     long get(int key);
 
+    /**
+     * @return current size of map(count of element that map contains)
+     */
     int size();
 
+    /**
+     * @return current map capacity
+     */
     int capacity();
 }
